587aaf71852a8d7c7b3c4a70adbf96285b824af9
Sverre Holm <la3za@users.noreply.github.com>
2020-04-12 20:39:56 +0200

    Update clock_misc.h

--- clock_misc.h
+++ clock_misc.h
@@ -11,3 +11,16 @@ Timezone CE(CEST, CET);
 #define LCD_NORSK
 
 //#define FEATURE_SERIAL_OUTPUT
+
+// display order of menus
+//int noOfStates = 12; // one more, i.e. 9, 2.7.2018; 10 on 12.09.2018
+
+//int menuOrder[] = {0,1,2,3,4,5,6,7,8,9,10,11}; // as many as noOfStates
+
+// NCDXF beacons first:
+//int menuOrder[] = {5,6,2,3,4,0,1,7,8,9,10,11}; // as many as noOfStates
+
+
+// testing moon rise time
+int noOfStates = 10;
+int menuOrder[] = {4,8,2,3,0,5,6,7,1,9,10,11}; // as many as noOfStates
